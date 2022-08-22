8738f777bd9adbd1f4fee75b09497f48b371dd79
leonardo <leonardo2322@gmail.com>
2022-08-22 07:33:36 -0500

    cambios de look

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const contImage = document.querySelector('.cont-image')
-const showImageIMG = document.querySelector('.show-image')
-const allImages = document.querySelectorAll('.img')
+const contImage = document.querySelector(".cont-image");
+const showImageIMG = document.querySelector(".show-image");
+const allImages = document.querySelectorAll(".img");
 
-allImages.forEach(image =>{
-    image.addEventListener('click',()=>{
-        
-        addImageLightbox(image.getAttribute('src'))
-    })
-})
+allImages.forEach((image) => {
+  image.addEventListener("click", () => {
+    addImageLightbox(image.getAttribute("src"));
+  });
+});
 
-const addImageLightbox = (src)=>{
-    
-    contImage.classList.toggle('move')
-    showImageIMG.src = src
-}
-contImage.addEventListener('click',()=>{
-    contImage.classList.toggle('move')
-})
+const addImageLightbox = (src) => {
+  contImage.classList.toggle("move");
+  showImageIMG.src = src;
+};
+contImage.addEventListener("click", () => {
+  contImage.classList.toggle("move");
+});
+
